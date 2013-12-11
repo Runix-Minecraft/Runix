5c00ff3bdedc3a25ff54eddd7cc9d69d9fa4b8f2
rivkiinshadows <rivkiinshadows@gmail.com>
2013-12-11 18:48:41 -0500

    Fixed Typo In Commit.

--- com/newlinegaming/Runix/Runix.java
+++ com/newlinegaming/Runix/Runix.java
@@ -2,16 +2,57 @@ package com.newlinegaming.Runix;
 
 	//Imports
 
+import com.newlinegaming.Runix.creativetabs.TabRunix;
+import com.newlinegaming.Runix.placeholder.RunixPlaceHolder;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.EnumArmorMaterial;
+import net.minecraft.item.Item;
+import net.minecraftforge.common.EnumHelper;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
-
-@Mod(modid = "Runix", name = "Runix", version = "0.1 Alpha")
-@NetworkMod(clientSideRequired = false, serverSideRequired = false)
+import cpw.mods.fml.common.registry.LanguageRegistry;
 
 
-public class Runix {
-	@SidedProxy(clientSide = "RuneCo.mod.Runix.common.ClientProxy", serverSide = "RuneCo.mod.Runix.common.CommonProxy")
-	//Josiah: Should this instead be com.newlinegaming.Runix.ClientProxy ?
+	//Client and Server
+
+	@Mod(modid = "Runix", name = "Runix", version = "0.1 Alpha")
+	@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+
+	public class Runix {
+	
+	@SidedProxy(clientSide = "com.newlinegaming.Runix.ClientProxy", serverSide = "com.newlinegaming.Runix.CommonProxy")
 	public static CommonProxy proxy;
+	
+	//EnumMaterial
+	
+	public static EnumArmorMaterial armorImmersed = EnumHelper.addArmorMaterial("IMMERSED", 66, new int[]{6, 9, 9, 6}, 30);
+			
+	//Creative Tab Names
+	
+	public static CreativeTabs TabRunix = new TabRunix(CreativeTabs.getNextID(), "Runix");
+
+	//Creative Tab Placeholder
+
+	public static Item RunixPlaceHolder = new RunixPlaceHolder(2000).setUnlocalizedName("RunixPlaceHolder");
+	public static Object instance;
+
+	//Render Information
+	
+	@Init
+	public void load(FMLInitializationEvent event) {
+		proxy.registerRenderInformation();
+}
+
+	public Runix() {
+	
+	//Language Registry
+
+	LanguageRegistry.addName(RunixPlaceHolder, "Runix");
+	
+	
+	}
 }
