ab7c8218bcc13450969b8c3e3e606f0c9372af7c
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2013-12-14 23:30:14 -0700

    Actually recognizing the Compass rune pattern!

--- forge/mcp/src/minecraft/com/newlinegaming/Runix/Compass.java
+++ forge/mcp/src/minecraft/com/newlinegaming/Runix/Compass.java
@@ -10,6 +10,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.Action;
 
 
 public class Compass// extends Block //extend Rune
@@ -27,21 +28,19 @@ public class Compass// extends Block //extend Rune
 	@ForgeSubscribe
 	public void playerInteractEvent(PlayerInteractEvent event)
 	{
-//		MinecraftServer.getServer().getConfigurationManager().sendPacketToAllPlayers(new Packet3Chat("Someone touched Something!", true));
-//		Runix.proxy.printMessageToPlayer("Someone touched Something!");
-		aetherSay("Someone touched Something!");
-		possibleRuneActivation(event.entity.worldObj, event.x, event.y, event.z);
+		if(event.action == Action.RIGHT_CLICK_BLOCK)
+			possibleRuneActivationEvent(event.entity.worldObj, event.x, event.y, event.z);
 	}
 	
     public void aetherSay(String msg) {
+//		Runix.proxy.printMessageToPlayer("Someone touched Something!");
+		/** TODO: The double messages can be fixed by using a proper Proxy (client side only, I think)
+		 * See: https://github.com/denoflionsx/GateCopy/blob/master/src/denoflionsx/GateCopy/Proxy/ProxyClient.java		 
+		 * Runix.clientSide?  */
     	Minecraft.getMinecraft().thePlayer.addChatMessage(msg);
 	}
 
-	/**
-     * Called whenever the block is added into the world. Args: world, x, y, z
-     */
-//    @Override
-    public void possibleRuneActivation(World world, int worldX, int worldY, int worldZ) { //TODO: world should be type World
+    public void possibleRuneActivationEvent(World world, int worldX, int worldY, int worldZ) { //TODO: world should be type World
         boolean createdRune = checkForAnyRunePattern(world, worldX, worldY, worldZ);
         if(createdRune)
         	aetherSay("Rune Created");
@@ -49,16 +48,20 @@ public class Compass// extends Block //extend Rune
 
     private boolean checkForAnyRunePattern(World world, int worldX, int worldY, int worldZ)
     {
-        for(int y = 0; y < blockPattern.length; y++)
-        {
-            for(int z = 0; z < blockPattern[y].length; z++){
-                
-                for(int x = 0; x < blockPattern[y][z].length; x++)
-                {
+		for (int y = 0; y < blockPattern.length; y++) {
+			for (int z = 0; z < blockPattern[y].length; z++) {
+				for (int x = 0; x < blockPattern[y][z].length; x++) {
                     //World coordinates + relative offset + half the size of the rune (for middle)
+					int blockX = worldX - blockPattern[y][z].length /2 + x;
+					int blockY = worldY - blockPattern.length / 2 + y;
+					int blockZ = worldZ - blockPattern[y].length /2 + z;
+					 if (world.getBlockId(blockX, blockY, blockZ) != blockPattern[y][z][x]) {
+						 aetherSay("Found" + world.getBlockId(blockX, blockY, blockZ) + " expected " + blockPattern[y][z][x]);
+						 return false;
+					 }
                 }
             }
         }
-        return false;//TODO: return true once non-empty checking is done
+        return true;//TODO: return true once non-empty checking is done
     }
 }
