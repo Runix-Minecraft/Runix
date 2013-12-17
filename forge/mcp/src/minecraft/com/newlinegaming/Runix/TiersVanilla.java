3b21a20fbcc6c3f857da375b5e51a926a2e9c07b
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2013-12-16 23:36:12 -0700

    Minor: comment in TiersVanilla

--- forge/mcp/src/minecraft/com/newlinegaming/Runix/TiersVanilla.java
+++ forge/mcp/src/minecraft/com/newlinegaming/Runix/TiersVanilla.java
@@ -2,12 +2,14 @@ package com.newlinegaming.Runix;
 
 
 
+import net.minecraft.block.Block; // Josiah: I added these imports, They should help
+import net.minecraft.block.material.Material;
 import java.util.ArrayList;
 
 
-
-
 public class TiersVanilla {
+    //Josiah: For naming block ids use the vanilla constants instead of hand typing from the wiki.
+    //Example:   Tier2.add(Block.torchWood.blockID);  or Block.sandStone.blockID
     
     
     
