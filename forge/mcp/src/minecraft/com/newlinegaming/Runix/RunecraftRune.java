a2bbed7f64f91ddd6a19feb185dbb9380c2ca0e8
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2013-12-18 23:11:06 -0700

    A working Runecraft that moves a vehicle with a 32 block radius 5m up.

--- forge/mcp/src/minecraft/com/newlinegaming/Runix/RunecraftRune.java
+++ forge/mcp/src/minecraft/com/newlinegaming/Runix/RunecraftRune.java
@@ -1,6 +1,7 @@
 package com.newlinegaming.Runix;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 
 import net.minecraft.block.Block;
@@ -24,11 +25,26 @@ public class RunecraftRune extends AbstractRune {
     @Override
     public void execute(EntityPlayer player, WorldCoordinates coords) {
         accept(player);
-        HashSet<WorldCoordinates> vehicleBlocks = conductanceStep(coords);
+        int tier = 4;
+        HashSet<WorldCoordinates> vehicleBlocks = conductanceStep(coords, (int)Math.pow(2, tier+1));
         aetherSay(player, "Found " + vehicleBlocks.size() + " tier blocks");
+        safelyMovePlayer(player, coords.offset(0, 5, 0));
+        moveShape(vehicleBlocks, 0, 5, 0); //Josiah: I'm not sure if we should move the player or blocks first
     }
 
-    private HashSet<WorldCoordinates> conductanceStep(WorldCoordinates startPoint) {
+    private void moveShape(HashSet<WorldCoordinates> vehicleBlocks, int dX, int dY, int dZ) {
+        //map each WorldCoordinate to a blockID
+        HashMap<WorldCoordinates, Integer> savedPattern = new HashMap<WorldCoordinates, Integer>();
+        for(WorldCoordinates loc : vehicleBlocks){
+            savedPattern.put(loc, new Integer(loc.getBlockId()));
+            loc.setBlockId(0); // delete old block
+        }
+        //offset(0,0,1) all coordinates //set to blockID
+        for(WorldCoordinates start : savedPattern.keySet())
+            start.offset(dX, dY, dZ).setBlockId(savedPattern.get(start)); //place new block 1 to the South
+    }
+
+    private HashSet<WorldCoordinates> conductanceStep(WorldCoordinates startPoint, int maxDistance) {
         //TODO: perhaps rename WorldCoordinates to WorldXYZ
         HashSet<WorldCoordinates> workingSet = new HashSet<WorldCoordinates>();
         HashSet<WorldCoordinates> activeEdge;
@@ -36,13 +52,15 @@ public class RunecraftRune extends AbstractRune {
         workingSet.add(startPoint);
         nextEdge.add(startPoint);
         
-        for(int iterationStep = 3; iterationStep > 0; iterationStep--) {
+        for(int iterationStep = maxDistance; iterationStep > 0; iterationStep--) {
             activeEdge = nextEdge;
             nextEdge = new HashSet<WorldCoordinates>();
             
             for(WorldCoordinates block : activeEdge) {
                 ArrayList<WorldCoordinates> neighbors = block.getNeighbors();
                 for(WorldCoordinates n : neighbors) {
+                    int blockID = n.getBlockId();
+                    // && blockID != 0 && blockID != 1){  // this is the Fun version!
                     if( !workingSet.contains(n) && !RuneHandler.tiers.isTier0(n.getBlockId()) ) {
                         workingSet.add(n);
                         nextEdge.add(n);
