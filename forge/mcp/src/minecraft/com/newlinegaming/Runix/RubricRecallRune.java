47b0ecc2a1267df102a6431c40b7f55d572bb856
blood-stone <blood-stone@cox.net>
2013-12-23 18:35:57 -0500

    new rune

--- forge/mcp/src/minecraft/com/newlinegaming/Runix/RubricRecallRune.java
+++ forge/mcp/src/minecraft/com/newlinegaming/Runix/RubricRecallRune.java
@@ -1,5 +1,35 @@
 package com.newlinegaming.Runix;
 
-public class RubricRecallRune {
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
 
+
+public class RubricRecallRune extends AbstractRune{
+
+	@Override
+		public int[][][] blockPattern() {
+			int RT=Block.torchRedstoneActive.blockID;
+			int AIR=0;
+			return new int [][][] 
+			            {{{TIER,TIER, AIR,TIER,TIER},
+			              {TIER, AIR,  RT, AIR,TIER},
+			              { AIR , RT,TIER,  RT, AIR},
+			              {TIER, AIR,  RT, AIR,TIER},
+			              {TIER,TIER, AIR,TIER,TIER}
+			             }}; 
+	}
+
+	@Override
+	public void execute(EntityPlayer player, WorldXYZ coords) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getRuneName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 }
