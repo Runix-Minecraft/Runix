b842e5629da9d4ce18f676204c78f16e3dd056c5
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2013-12-27 20:34:30 -0700

    Removed the Teleport to spawn functionality because it was annoying me

--- forge/mcp/src/minecraft/com/newlinegaming/Runix/TeleporterRune.java
+++ forge/mcp/src/minecraft/com/newlinegaming/Runix/TeleporterRune.java
@@ -11,40 +11,41 @@ public class TeleporterRune extends AbstractRune {
 		return new int[][][]
 				{{{NONE,TIER,SIGR,TIER,NONE},
 				  {TIER,TIER,TIER,TIER,TIER},
-				  {SIGR,TIER,NONE,TIER,SIGR},
+				  {SIGR,TIER,KEY ,TIER,SIGR},
 				  {TIER,TIER,TIER,TIER,TIER},
 				  {NONE,TIER,SIGR,TIER,NONE}}};
 	}
 	
 	@Override
-	/**This method moves the player to spawn.
-	 * WorldXYZ are not used.
+	/**Teleport the player to the WaypointRune with a matching signature
 	 */
 	public void execute(EntityPlayer player, WorldXYZ coords) {
 	    accept(player);
+//	    if(Tiers.getTier(coords.getBlockId()) > 0){
+//	        consumeKeyBlock(coords...)
+//	    }
+	    // if coords.equals(location)
+	    //     execute as normal
+	    // else
+	    //     find matching rune or construct new rune and pass off execution
+	    
 	    Signature signature = new Signature(this, coords);
 	    WorldXYZ destination;
 	    //This is necessary because getActiveMagic() CANNOT be static, so it returns a pointer to a static field...
 	    ArrayList<PersistentRune> waypointList = (new WaypointRune().getActiveMagic());
 	    
-		if( waypointList.isEmpty()){
-		    destination = new WorldXYZ(player.worldObj.getSpawnPoint());
-		    destination.worldObj = player.worldObj;
-		}
-		else{ 
-    	    PersistentRune wp = null;
-    	    for( PersistentRune candidate : waypointList){
-    	        if( new Signature(candidate, candidate.location).equals( signature ) ){
-    	            wp = candidate;
-    	            break;
-    	        }
-    	    }
-    	    if( wp == null){
-    	        aetherSay(player, "A waypoint with that signature cannot be found.");
-    	        return;
-    	    }
-    	    destination = new WorldXYZ(wp.location);
-		}
+	    PersistentRune wp = null;
+	    for( PersistentRune candidate : waypointList){
+	        if( new Signature(candidate, candidate.location).equals( signature ) ){
+	            wp = candidate;
+	            break;
+	        }
+	    }
+	    if( wp == null){
+	        aetherSay(player, "A waypoint with that signature cannot be found.");
+	        return;
+	    }
+	    destination = new WorldXYZ(wp.location);
 		safelyMovePlayer(player, destination);
 	}
 
