0953ca7396905d362df7520f1db1df227d8521ab
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-01-04 10:18:35 -0700

    Added hook for PersistentRune to respond to player logging in: onPlayerLogin(String username)  At the moment it's not really needed.

--- forge/mcp/src/minecraft/com/newlinegaming/Runix/PersistentRune.java
+++ forge/mcp/src/minecraft/com/newlinegaming/Runix/PersistentRune.java
@@ -142,6 +142,7 @@ public abstract class PersistentRune extends AbstractRune{
     }
     
     public boolean onPlayerLogin(String username){
+//        System.out.println(username + " joined the server");
         return false;
     }
     
