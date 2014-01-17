c5dc86f34e0f48d433b8c205d0ef42e7d372ec59
rivkiinshadows <rivkiinshadows@gmail.com>
2014-01-17 03:57:11 -0500

    Fixed render issue for GreekFire. Working on making it so fire and GreekFire are not affecting same blocks.

--- forge/mcp/src/minecraft/com/newlinegaming/Runix/block/GreekFire.java
+++ forge/mcp/src/minecraft/com/newlinegaming/Runix/block/GreekFire.java
@@ -12,59 +12,38 @@ import net.minecraft.util.Icon;
 
 public class GreekFire extends BlockFire {
 
-    //@SideOnly(Side.CLIENT)
-    //private Icon[] iconArray;
-
     public GreekFire(int par1) {
 	super(par1);
 
-	setTickRandomly(true);
 	setCreativeTab(Runix.TabRunix);
-
-    }
-
-    //public Icon getIcon(int par1, int par2)
-    //{
-	//return this.blockIcon;
-    //}
-
-    public void initializeBlock()
-    {
-	this.blockHardness = 100.0F;
-	this.setLightOpacity(15);
-	this.setLightValue(15.0F);
+	this.setLightValue(1.0f);
 	this.setBurnProperties(Block.stone.blockID, 30, 60);
 	this.setBurnProperties(Block.grass.blockID, 30, 60);
 	this.setBurnProperties(Block.dirt.blockID, 30, 60);
 	this.setBurnProperties(Block.gravel.blockID, 30, 60);
     }
+    
+    @SideOnly(Side.CLIENT)
+    private Icon[] iconArray;
+    
+    @SideOnly(Side.CLIENT)
 
-    //public boolean isOpaqueCube()
-    //{
-	//return false;
-   // }
-   // public boolean renderAsNormalBlock()
-   // {
-	//return false;
-   // }
-   // public int getRenderType()
-   // {
-	//return 3;
-   // }
+    public void registerIcons(IconRegister par1IconRegister)
+    {
+        this.iconArray = new Icon[] {par1IconRegister.registerIcon("Runix:GreekFire"), par1IconRegister.registerIcon("Runix:GreekFire1")};
+    }
 
-   // @SideOnly(Side.CLIENT)
-   // public void registerIcons(IconRegister par1IconRegister)
-   // {
-//	this.iconArray = new Icon[] { par1IconRegister.registerIcon("Runix:GreekFire"), par1IconRegister.registerIcon("Runix:GreekFire1") };
-   // }
-   // @SideOnly(Side.CLIENT)
-   // public Icon func_94438_c(int par1) {
-//	return this.iconArray[par1];
-   // }
-   // @SideOnly(Side.CLIENT)
-   // public Icon getBlockTextureFromSideAndMetadata(int par1, int par2)
-   // {
-//	return this.iconArray[0];
-   // }
+    @SideOnly(Side.CLIENT)
+    public Icon getFireIcon(int par1)
+    {
+        return this.iconArray[par1];
+    }
+
+    @SideOnly(Side.CLIENT)
+
+    public Icon getIcon(int par1, int par2)
+    {
+        return this.iconArray[0];
+    }
 
 }
