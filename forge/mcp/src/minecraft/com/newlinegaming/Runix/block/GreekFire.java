503d86eaf2745a01342d6a364924751a0f6d01e7
rivkiinshadows <rivkiinshadows@gmail.com>
2014-01-17 02:42:22 -0500

    Commented out Rendering for GreekFire, due to bug.

--- forge/mcp/src/minecraft/com/newlinegaming/Runix/block/GreekFire.java
+++ forge/mcp/src/minecraft/com/newlinegaming/Runix/block/GreekFire.java
@@ -12,33 +12,59 @@ import net.minecraft.util.Icon;
 
 public class GreekFire extends BlockFire {
 
+    //@SideOnly(Side.CLIENT)
+    //private Icon[] iconArray;
+
     public GreekFire(int par1) {
 	super(par1);
-	
-        this.blockHardness = 100.0F;
-        this.setLightOpacity(15);
-        this.setCreativeTab(Runix.TabRunix);
-        this.setLightValue(15.0F);
-        this.setBurnProperties(Block.stone.blockID, 30, 60);
-        this.setBurnProperties(Block.grass.blockID, 30, 60);
-        this.setBurnProperties(Block.dirt.blockID, 30, 60);
-        this.setBurnProperties(Block.gravel.blockID, 30, 60);
+
+	setTickRandomly(true);
+	setCreativeTab(Runix.TabRunix);
+
     }
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void registerIcons(IconRegister par1IconRegister)
+
+    //public Icon getIcon(int par1, int par2)
+    //{
+	//return this.blockIcon;
+    //}
+
+    public void initializeBlock()
     {
-    this.setIconArray(new Icon[] { par1IconRegister.registerIcon("Runix:GreekFire"), par1IconRegister.registerIcon("Runix:GreekFire1") });
+	this.blockHardness = 100.0F;
+	this.setLightOpacity(15);
+	this.setLightValue(15.0F);
+	this.setBurnProperties(Block.stone.blockID, 30, 60);
+	this.setBurnProperties(Block.grass.blockID, 30, 60);
+	this.setBurnProperties(Block.dirt.blockID, 30, 60);
+	this.setBurnProperties(Block.gravel.blockID, 30, 60);
     }
-    @SideOnly(Side.CLIENT)
-    public Icon func_94438_c(int par1) {
-    return this.getIconArray()[par1];
-    }
-    @SideOnly(Side.CLIENT)
-    public Icon getBlockTextureFromSideAndMetadata(int par1, int par2)
-    {
-    return this.getIconArray()[0];
-    }
-    
+
+    //public boolean isOpaqueCube()
+    //{
+	//return false;
+   // }
+   // public boolean renderAsNormalBlock()
+   // {
+	//return false;
+   // }
+   // public int getRenderType()
+   // {
+	//return 3;
+   // }
+
+   // @SideOnly(Side.CLIENT)
+   // public void registerIcons(IconRegister par1IconRegister)
+   // {
+//	this.iconArray = new Icon[] { par1IconRegister.registerIcon("Runix:GreekFire"), par1IconRegister.registerIcon("Runix:GreekFire1") };
+   // }
+   // @SideOnly(Side.CLIENT)
+   // public Icon func_94438_c(int par1) {
+//	return this.iconArray[par1];
+   // }
+   // @SideOnly(Side.CLIENT)
+   // public Icon getBlockTextureFromSideAndMetadata(int par1, int par2)
+   // {
+//	return this.iconArray[0];
+   // }
 
 }
