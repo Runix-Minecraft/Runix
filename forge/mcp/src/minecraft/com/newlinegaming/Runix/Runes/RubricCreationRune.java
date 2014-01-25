50ae5c7359ebbf9abbde25c9c42c0bd97ca7d3ba
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-01-25 16:47:40 -0700

    Cleaning up RubricCreationRune.java

--- forge/mcp/src/minecraft/com/newlinegaming/Runix/Runes/RubricCreationRune.java
+++ forge/mcp/src/minecraft/com/newlinegaming/Runix/Runes/RubricCreationRune.java
@@ -19,30 +19,13 @@ import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ForgeSubscribe;
 
-//import java.io.InputStream;
-//import java.io.InputStreamReader;
-//import java.util.HashMap;
-//import java.util.Map;
-//import java.util.logging.Level;
-//
-//import argo.jdom.JdomParser;
-//import argo.jdom.JsonNode;
-//import argo.jdom.JsonRootNode;
-//import argo.saj.InvalidSyntaxException;
-//
-//import net.minecraft.block.Block;
-//import net.minecraft.entity.player.EntityPlayer;
-//import net.minecraft.nbt.NBTTagCompound;
-//import net.minecraft.nbt.NBTTagList;
-//import net.minecraft.nbt.NBTTagString;
-//import net.minecraft.world.World;
 
 public class RubricCreationRune extends PersistentRune {
 
 	private static ArrayList<PersistentRune> storedPatterns = new ArrayList<PersistentRune>();
-	public Signature sig=null;
-	public HashMap<WorldXYZ, SigBlock> structure=new HashMap<WorldXYZ, SigBlock>();
-	protected transient RenderHelper renderer;
+	public Signature sig = null;
+	public HashMap<WorldXYZ, SigBlock> structure = new HashMap<WorldXYZ, SigBlock>();
+	protected transient RenderHelper renderer = null;
 
     public RubricCreationRune() {
         runeName = "Rubric Creator";
@@ -51,10 +34,12 @@ public class RubricCreationRune extends PersistentRune {
     public RubricCreationRune(WorldXYZ coords, EntityPlayer player2) 
     {
 	    super(coords, player2,"Rubric Creator");
-		renderer = new RenderHelper();
-		MinecraftForge.EVENT_BUS.register(this);
-		
 	}
+    
+    protected void initializeRune(){
+        renderer = new RenderHelper();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
 
     private HashMap<WorldXYZ, SigBlock> scanStructure(HashSet<WorldXYZ> shape) {
         HashMap<WorldXYZ, SigBlock> fullData = new HashMap<WorldXYZ, SigBlock>();
@@ -77,11 +62,12 @@ public class RubricCreationRune extends PersistentRune {
 
 	@Override
 	protected void poke(EntityPlayer poker, WorldXYZ coords){
+	    if( renderer == null)
+	        initializeRune();
 		renderer.reset();
 		HashSet<WorldXYZ> shape = conductanceStep(coords, 50);
 		structure = scanStructure(shape);
 		ItemStack toolused = poker.getCurrentEquippedItem();
-		specialName = toolused.getDisplayName();
 		sig = new Signature(this, coords);
 				//TODO check for signature collision
 
@@ -92,7 +78,7 @@ public class RubricCreationRune extends PersistentRune {
 			structure=scanStructure(shape);//then capture everything else into the rubric file 
 			consumeRune(structure.keySet());
 
-			
+	        specialName = toolused.getDisplayName();
 			aetherSay(poker, "the tool used is "+ specialName);
 
 
@@ -109,21 +95,12 @@ public class RubricCreationRune extends PersistentRune {
 			renderer.highlightBoxes(structure.keySet(), false, getPlayer(), 221, 0, 0);
 
 	}
-
-//	@Override
-//    public void saveActiveRunes() {
-//        //Do nothing
-//    }
 	
     @Override
     public void loadRunes() {
-        //Do nothing
+        super.loadRunes();
+        System.out.println("Rubrics: " + getActiveMagic().size() + "\n    " + getActiveMagic());
     }	
-
-    @Override
-	public String getRuneName() {
-		return this.runeName;
-	}
 
     @Override
     public ArrayList<PersistentRune> getActiveMagic() {
