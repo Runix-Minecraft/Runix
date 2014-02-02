990778928481b9ac76e3782f0ed17a2016daf79f
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-02-01 19:09:50 -0700

    Faith: Removed the class variable sphere for memory reasons

--- forge/mcp/src/minecraft/com/newlinegaming/Runix/Runes/FaithRune.java
+++ forge/mcp/src/minecraft/com/newlinegaming/Runix/Runes/FaithRune.java
@@ -22,7 +22,7 @@ public class FaithRune extends PersistentRune{
 	
 	protected static ArrayList<PersistentRune> activeFaithList = new ArrayList<PersistentRune>();
 	public Integer radius = 11;
-	public LinkedList<WorldXYZ> sphere = null;
+    private boolean firstTime;
 	
 	public FaithRune(){
 	    runeName = "Faith";
@@ -30,6 +30,7 @@ public class FaithRune extends PersistentRune{
 	public FaithRune(WorldXYZ loc, EntityPlayer creator)
 	{
 	    super(loc, creator, "Faith");
+	    firstTime = true;
 	}
 	
 	public int[][][] runicTemplateOriginal(){
@@ -64,18 +65,24 @@ public class FaithRune extends PersistentRune{
 	
     @Override
     protected void poke(EntityPlayer poker, WorldXYZ coords) {
-        consumeRune(coords);
-        try {
-            setBlockIdAndUpdate(coords, Block.blockGold.blockID); //Gold block is to be a permanent marker
-        } catch (NotEnoughRunicEnergyException e) {}
-        radius = Tiers.energyToRadiusConversion(energy);
-        sphere = Util_SphericalFunctions.getSphere(coords, radius);
-        energy -= sphere.size() * Tiers.blockMoveCost;
+        if(firstTime){
+            firstTime = false;
+            consumeRune(coords);
+            try {
+                setBlockIdAndUpdate(coords, Block.blockGold.blockID); //Gold block is to be a permanent marker
+            } catch (NotEnoughRunicEnergyException e) {}
+            radius = Tiers.energyToRadiusConversion(energy);
+            LinkedList<WorldXYZ> sphere = Util_SphericalFunctions.getSphere(coords, radius);
+            energy -= sphere.size() * Tiers.blockMoveCost;
+            aetherSay(poker, "Created a Faith Sphere with " + sphere.size() + " blocks.");
+            return;
+        }
         bounceIsland();
     }
     
     public void bounceIsland()
 	{
+        LinkedList<WorldXYZ> sphere = Util_SphericalFunctions.getSphere(location, radius);
 		HashMap<WorldXYZ, WorldXYZ> moveMapping = Util_Movement.displaceShape(sphere, 0, radius*2+1, 0);
 		Util_Movement.performMove(moveMapping);//this is the version that doesn't cost energy, Faith pays the cost up front
 	}
