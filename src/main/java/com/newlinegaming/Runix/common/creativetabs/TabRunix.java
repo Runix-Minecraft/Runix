dd1c5ccea6a3f20fe17a630e2fcea8c83fa2d716
LordIllyohs <lordillyohs@gmail.com>
2014-05-07 18:04:13 -0400

    Changed the Icon

--- src/main/java/com/newlinegaming/Runix/common/creativetabs/TabRunix.java
+++ src/main/java/com/newlinegaming/Runix/common/creativetabs/TabRunix.java
@@ -3,7 +3,6 @@ package com.newlinegaming.Runix.common.creativetabs;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
-import com.newlinegaming.Runix.common.fluids.ModFluid;
 import com.newlinegaming.Runix.common.item.ModItem;
 
 public class TabRunix extends CreativeTabs {
