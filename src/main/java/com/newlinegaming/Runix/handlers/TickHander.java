034fa177cde3acb81e4eeb34b48ec85a0c37552b
LordIllyohs <lordillyohs@gmail.com>
2014-05-20 01:33:00 -0400

    Prep

---
+++ src/main/java/com/newlinegaming/Runix/handlers/TickHander.java
@@ -0,0 +1,31 @@
+package com.newlinegaming.Runix.handlers;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
+
+/**
+ * @author Anthony Anderson(LordIllyohs)
+ *         File created on May, 19 2014
+ */
+public class TickHander {
+
+    @SubscribeEvent
+    public void onWorldTickEvent(TickEvent.WorldTickEvent event){
+
+    }
+
+    @SubscribeEvent
+    public void onServerTickEvent(TickEvent.ServerTickEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public void onPlayerTickEvent(TickEvent.PlayerTickEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public void onClientTickEvent(TickEvent.ClientTickEvent event) {
+
+    }
+}
