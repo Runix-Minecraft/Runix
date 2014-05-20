2425ffbc9a77f2916a831f132969bf538fc873d5
LordIllyohs <lordillyohs@gmail.com>
2014-05-19 23:11:01 -0400

    Made a LogHelper

---
+++ src/main/java/com/newlinegaming/Runix/helper/LogHelper.java
@@ -0,0 +1,32 @@
+package com.newlinegaming.Runix.helper;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.newlinegaming.Runix.lib.LibRef;
+
+public class LogHelper {
+
+    private static final Logger RLog = LogManager.getLogger(LibRef.MOD_ID);
+
+    public static void log(Level logLevel, Object obj) {
+        RLog.log(logLevel, obj.toString());
+    }
+
+    public static void debug(Object obj) {
+        log(Level.DEBUG, obj.toString());
+    }
+
+    public static void warn(Object obj) {
+        log(Level.WARN, obj.toString());
+    }
+
+    public static void info(Object obj) {
+        log(Level.INFO, obj.toString());
+    }
+
+    public static void fatal(Object obj) {
+        log(Level.INFO, obj.toString());
+    }
+}
