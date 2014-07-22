2840be2446b2362f2328b262e0a62a232b77657e
Anthony Anderson <LordIllyohs@gmail.com>
2014-07-22 00:20:18 -0400

    Forgot to comment out the EventHandlerWorld
    Signed-off-by: Anthony Anderson <LordIllyohs@gmail.com>

--- src/main/java/com/newlinegaming/Runix/RunixMain.java
+++ src/main/java/com/newlinegaming/Runix/RunixMain.java
@@ -54,7 +54,7 @@ public class RunixMain {
     @EventHandler
     public void load(FMLInitializationEvent event) {
         proxy.registerRenderInformation();
-        FMLCommonHandler.instance().bus().register(new EventHandlerWorld());
+//        FMLCommonHandler.instance().bus().register(new EventHandlerWorld());
     }
 
     //Registry's
