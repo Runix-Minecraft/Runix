128b6d552a7ee4d191953106226a6f4e6ff3bd0e
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-07-23 00:22:46 -0600

    Created spaceholder Block classes.

---
+++ src/main/java/com/newlinegaming/Runix/block/SignatureBlock.java
@@ -0,0 +1,15 @@
+package com.newlinegaming.Runix.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class SignatureBlock extends Block
+{
+
+    public SignatureBlock()
+    {
+        super(Material.air);
+        setBlockName("SIGR");
+    }
+
+}
---
+++ src/main/java/com/newlinegaming/Runix/block/TierBlock.java
@@ -0,0 +1,15 @@
+package com.newlinegaming.Runix.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class TierBlock extends Block
+{
+
+    public TierBlock()
+    {
+        super(Material.air);
+        setBlockName("TIER");
+    }
+
+}
