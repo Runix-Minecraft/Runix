ccd4822f3d494aeb1ed193cf49b83ae440bccd27
Anthony Anderson <LordIllyohs@gmail.com>
2014-07-24 23:35:53 -0400

    Removed Junit import
    Signed-off-by: Anthony Anderson <LordIllyohs@gmail.com>

--- src/main/java/com/newlinegaming/Runix/Tests.java
+++ src/main/java/com/newlinegaming/Runix/Tests.java
@@ -1,21 +1,16 @@
 package com.newlinegaming.Runix;
 
-import static org.junit.Assert.assertTrue;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 
-import org.junit.Test;
+public class Tests {
 
-public class Tests
-{
-
-    @Test
-    public void SigBlockEqualityTest()
-    {
-        Block a = Blocks.redstone_wire;
-        SigBlock b = new SigBlock(a, 0);
+//    @Test
+//    public void SigBlockEqualityTest() Cd{
+//        Block a = Blocks.redstone_wire;
+//        SigBlock b = new SigBlock(a, 0);
 //        assertTrue( a == b );
-        assertTrue( b.equals(a) );
-    }
+//        assertTrue( b.equals(a) );
+//    }
 
 }
