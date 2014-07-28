174c2672137a264e1d633b8a0205100e25187c44
Anthony Anderson <LordIllyohs@gmail.com>
2014-07-28 14:19:34 -0400

    Added a fix me
    Signed-off-by: Anthony Anderson <LordIllyohs@gmail.com>

--- src/main/java/com/newlinegaming/Runix/handlers/RuneHandler.java
+++ src/main/java/com/newlinegaming/Runix/handlers/RuneHandler.java
@@ -61,7 +61,7 @@ public class RuneHandler {
         runeRegistry.add(new CompassRune());
         runeRegistry.add(new FtpRune());
         runeRegistry.add(new TeleporterRune());
-        runeRegistry.add(new RunecraftRune());
+        runeRegistry.add(new RunecraftRune()); //FIXME: Make Runecraft runes repsect TileEntity Inventories 
 //        runeRegistry.add(new RubricCreationRune());
 //        runeRegistry.add(new RubricRecallRune());
         runeRegistry.add(new TorchBearerRune());
