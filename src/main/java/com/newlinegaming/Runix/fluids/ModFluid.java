d39fc94bfc5b40ee67eace813c62087672991e14
Anthony Anderson <LordIllyohs@gmail.com>
2014-08-03 01:56:35 -0400

    Reimplemented QixSilver
    Signed-off-by: Anthony Anderson <LordIllyohs@gmail.com>

--- src/main/java/com/newlinegaming/Runix/fluids/ModFluid.java
+++ src/main/java/com/newlinegaming/Runix/fluids/ModFluid.java
@@ -1,5 +1,6 @@
 package com.newlinegaming.Runix.fluids;
 
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraftforge.fluids.Fluid;
@@ -12,7 +13,7 @@ public class ModFluid {
 
     public static void init() {
 
-//Fluids
+        //Fluids
         QixSilver = new FluidQixSilver();
 
         //Fluid Blocks
@@ -23,6 +24,6 @@ public class ModFluid {
 
     private static void GameReg() {
 
-//        GameRegistry.registerBlock(BlockQuickSilver, "runixqiicksilver");
+                GameRegistry.registerBlock(BlockQixSilver, "runixqixsilver");
     }
 }
