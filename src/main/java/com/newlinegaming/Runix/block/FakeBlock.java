327b983c2d6cf4cf06598fb89bc5c5e1bd7ef235
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-08-08 20:34:11 -0600

    Fixed texture bug on FakeBlock.  You'll need to add new texture names.

--- src/main/java/com/newlinegaming/Runix/block/FakeBlock.java
+++ src/main/java/com/newlinegaming/Runix/block/FakeBlock.java
@@ -1,16 +1,23 @@
 package com.newlinegaming.Runix.block;
 
+import java.util.HashMap;
+
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
+
+
 
 public class FakeBlock extends Block {
 
+    HashMap<Block, String> textures = new HashMap<Block, String>();
+
     public FakeBlock(Block mimic) {
-        super(Material.iron);
+        super(mimic.getMaterial());
+        textures.put(Blocks.gold_block, "minecraft:gold_block");
         setBlockUnbreakable();
         setBlockName("runix:faith_anchor");
         try{
-            setBlockTextureName(mimic.getUnlocalizedName()); //"minecraft:gold_block");
+            setBlockTextureName(textures.get(mimic)); //"minecraft:gold_block");
         }
         catch (Throwable t) { }//default to Iron
     }
