509977968070b80d04cfa8b4f89c4e4c9a3f987a
Anthony Anderson <LordIllyohs@gmail.com>
2014-08-21 01:59:51 -0400

    Oracle is no longer a persistent rune
    Signed-off-by: Anthony Anderson <LordIllyohs@gmail.com>

--- src/main/java/com/newlinegaming/Runix/rune/OracleRune.java
+++ src/main/java/com/newlinegaming/Runix/rune/OracleRune.java
@@ -4,79 +4,98 @@ import java.util.ArrayList;
 
 import com.newlinegaming.Runix.PersistentRune;
 import com.newlinegaming.Runix.handlers.RuneHandler;
+import com.newlinegaming.Runix.AbstractRune;
 import com.newlinegaming.Runix.Tiers;
 import com.newlinegaming.Runix.WorldXYZ;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockRedstoneWire;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumChatFormatting;
 
-public class OracleRune extends PersistentRune {
-	private static ArrayList<PersistentRune> activeOracles = new ArrayList<PersistentRune>();
-	public OracleRune() {
-		runeName = ("Oracle Rune");
-	}
+public class OracleRune extends AbstractRune {
+    
+    public OracleRune() {
+        runeName = ("Oracle Rune");
+    }
 
-	public OracleRune(WorldXYZ coords, EntityPlayer player) {
-		super(coords, player, "Oracle Rune");
-	}
+    @Override
+    protected Block[][][] runicTemplateOriginal() {
+        
+        Block RED = Blocks.redstone_wire;
+        
+        return new Block[][][] {{
+            {RED, RED ,RED},
+            {RED, TIER, RED},
+            {RED, RED, RED}
+            
+        }};
+    }
 
-	@Override
-	public ArrayList<PersistentRune> getActiveMagic() {
-		return activeOracles;
-	}
+    @Override
+    public boolean isFlatRuneOnly() {
+        return true;
+    }
 
-	@Override
-	public boolean oneRunePerPerson() {
-		return false;
-	}
-
-	@Override
-	protected Block[][][] runicTemplateOriginal() {
-
-		Block RED = Blocks.redstone_wire;
-		return new Block[][][] {{
-			{RED,RED,RED},
-			{RED,TIER,RED},
-			{RED,RED,RED}
-		}};
-	}
-	
-	@Override
-	protected void poke(EntityPlayer player, WorldXYZ coords) {
-		ItemStack toolUsed = player.getHeldItem();
-		
-		if(toolUsed !=null && toolUsed.getItem() == Items.golden_sword || 
-		        toolUsed !=null && toolUsed.getItem() == Items.stone_sword || 
-		        toolUsed !=null && toolUsed.getItem() == Items.wooden_sword ||
-		        toolUsed !=null && toolUsed.getItem() == Items.diamond_sword) {
-			ArrayList<PersistentRune> d = RuneHandler.getInstance().getAllRunesByPlayer(player);
-			aetherSay(player, "Current enchantments: " + Integer.toString(d.size()));
-			for (PersistentRune r : d) {
-				aetherSay(player, r.runeName + " Energy: "+ r.energy);
-			}
-			
-		} else {
-			Block block = coords.getBlock();
-			
-			aetherSay(player, EnumChatFormatting.RED +block.getLocalizedName());
-			aetherSay(player, "Tier: "  + Tiers.getTier(block) + ".");
-			aetherSay(player, "Energy: " + Tiers.getEnergy(block) + ".");
-			aetherSay(player, "Properties: " + (Tiers.isNatural(block)? "Not Conductive" : "Conductive")
-					+ ", " + (Tiers.isCrushable(block)? "Crushable." : "Not Crushable."));
-		}
-	}
-
-	@Override
-	public boolean isFlatRuneOnly() {
-		return true;
-	}
-
-	
+    @Override
+    public void execute(WorldXYZ coords, EntityPlayer player) {
+      ItemStack toolUsed = player.getHeldItem();
+      
+      if(toolUsed !=null && toolUsed.getItem() == Items.golden_sword || 
+              toolUsed !=null && toolUsed.getItem() == Items.stone_sword || 
+              toolUsed !=null && toolUsed.getItem() == Items.wooden_sword ||
+              toolUsed !=null && toolUsed.getItem() == Items.diamond_sword) {
+          
+          ArrayList<PersistentRune> d = RuneHandler.getInstance().getAllRunesByPlayer(player);
+          aetherSay(player, "Current enchantments: " + Integer.toString(d.size()));
+          for (PersistentRune r : d) {
+              aetherSay(player, r.runeName + " Energy: "+ r.energy);
+          }
+          
+      } else {
+          
+          Block block = coords.getBlock();
+          
+          aetherSay(player, EnumChatFormatting.RED +block.getLocalizedName());
+          aetherSay(player, "Tier: "  + Tiers.getTier(block) + ".");
+          aetherSay(player, "Energy: " + Tiers.getEnergy(block) + ".");
+          aetherSay(player, "Properties: " + (Tiers.isNatural(block)? "Not Conductive" : "Conductive")
+                  + ", " + (Tiers.isCrushable(block)? "Crushable." : "Not Crushable."));
+      }
+    }
+    
+//	@Override
+//	protected void poke(EntityPlayer player, WorldXYZ coords) {
+//		ItemStack toolUsed = player.getHeldItem();
+//		
+//		if(toolUsed !=null && toolUsed.getItem() == Items.golden_sword || 
+//		        toolUsed !=null && toolUsed.getItem() == Items.stone_sword || 
+//		        toolUsed !=null && toolUsed.getItem() == Items.wooden_sword ||
+//		        toolUsed !=null && toolUsed.getItem() == Items.diamond_sword) {
+//			ArrayList<PersistentRune> d = RuneHandler.getInstance().getAllRunesByPlayer(player);
+//			aetherSay(player, "Current enchantments: " + Integer.toString(d.size()));
+//			for (PersistentRune r : d) {
+//				aetherSay(player, r.runeName + " Energy: "+ r.energy);
+//			}
+//			
+//		} else {
+//			Block block = coords.getBlock();
+//			
+//			aetherSay(player, EnumChatFormatting.RED +block.getLocalizedName());
+//			aetherSay(player, "Tier: "  + Tiers.getTier(block) + ".");
+//			aetherSay(player, "Energy: " + Tiers.getEnergy(block) + ".");
+//			aetherSay(player, "Properties: " + (Tiers.isNatural(block)? "Not Conductive" : "Conductive")
+//					+ ", " + (Tiers.isCrushable(block)? "Crushable." : "Not Crushable."));
+//		}
+//	}
+//
+//	@Override
+//	public boolean isFlatRuneOnly() {
+//		return true;
+//	}
+//
+//	
 
 }
