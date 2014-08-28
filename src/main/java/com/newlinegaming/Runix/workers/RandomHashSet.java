d404db7b75efcd9c45dc35e456fb083f773c0ef9
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-08-28 00:08:52 -0600

    Added orphan code for RandomHashSet.  Feel free to try using this in the Worker if you want less liney animations.

---
+++ src/main/java/com/newlinegaming/Runix/workers/RandomHashSet.java
@@ -0,0 +1,33 @@
+package com.newlinegaming.Runix.workers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+/** From http://stackoverflow.com/a/12386664/3067894 
+ * If we use this inside a worker it will give us truly random block iteration.*/
+public class RandomHashSet<A> {
+    ArrayList<A> contents = new ArrayList();
+    HashMap<A,Integer> indices = new HashMap<A,Integer>();
+    Random R = new Random();
+
+    //selects random element in constant time
+    A randomKey() {
+        return contents.get(R.nextInt(contents.size()));
+    }
+
+    //adds new element in constant time
+    void add(A a) {
+        indices.put(a,contents.size());
+        contents.add(a);
+    }
+
+    //removes element in constant time
+    void remove(A a) {
+        int index = indices.get(a);
+        contents.set(index,contents.get(contents.size()-1));
+        contents.remove(contents.size()-1);
+        indices.put(contents.get(contents.size()-1), index);
+        indices.remove(a);
+    }
+}
