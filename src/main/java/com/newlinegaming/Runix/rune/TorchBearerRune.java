e04e159a5f60f1620f3cf2b7ef64863b9a76c1b4
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-09-04 20:09:04 -0600

    Reduced the range on Torch Bearer to make it easier to back track for monster spawners.

--- src/main/java/com/newlinegaming/Runix/rune/TorchBearerRune.java
+++ src/main/java/com/newlinegaming/Runix/rune/TorchBearerRune.java
@@ -34,7 +34,7 @@ public class TorchBearerRune extends AbstractTimedRune {
     	if(subject.equals(getPlayer()) && !subject.worldObj.isRemote) {
             World world = subject.worldObj;//sphere can be optimized to donut
             location = new WorldXYZ(getPlayer());
-            HashSet<WorldXYZ> sphere = Util_SphericalFunctions.getShell(location, 3);
+            HashSet<WorldXYZ> sphere = Util_SphericalFunctions.getShell(location, 1);
             for(WorldXYZ newPos : sphere) {
                 if(newPos.getBlock().equals(Blocks.air) && 
                 	newPos.offset(Vector3.DOWN).getBlock().canPlaceTorchOnTop(world, newPos.posX, newPos.posY-1, newPos.posZ) && (
