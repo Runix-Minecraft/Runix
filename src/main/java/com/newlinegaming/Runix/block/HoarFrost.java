5e5549477e5f921c1f667f42468b77accf8adb6a
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2014-09-14 23:02:34 -0600

    Better rendering of Hoar Frost.  Still working on Origin block showing regularly when there are other blocks to the side.

--- src/main/java/com/newlinegaming/Runix/block/HoarFrost.java
+++ src/main/java/com/newlinegaming/Runix/block/HoarFrost.java
@@ -12,11 +12,10 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.event.ForgeEventFactory;
-import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 
 import com.newlinegaming.Runix.RunixMain;
 import com.newlinegaming.Runix.SigBlock;
@@ -33,13 +32,13 @@ public class HoarFrost extends BlockIce {
     public EntityPlayer owner = null; // updated by HoarFrostItem
 
     public HoarFrost() {
+//        super("ice", Material.ice, false);
         super();
         setTickRandomly(true);
         setCreativeTab(RunixMain.TabRunix);
         setHardness(0.5F);
         setStepSound(soundTypeGlass);
         setBlockName("runix:hoarfrost");
-        setBlockTextureName("ice_packed");
 //        setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.1225F, 1.0F);
     }
 
@@ -47,6 +46,42 @@ public class HoarFrost extends BlockIce {
     public int getRenderType() {
         return 72;
     }
+    
+    /**
+     * Returns whether this block is collideable based on the arguments passed in n
+     * @param par1 block metaData n
+     * @param par2 whether the player right-clicked while holding a boat*/
+    public boolean canCollideCheck(int meta, boolean p_149678_2_)
+    {
+        if(meta == 1 || meta == 3 || meta == 4 || meta == 15) //creep, stasis, explode delete
+            return false;
+        return true;
+    }
+    
+    public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x, int y, int z)
+    {
+        int meta = world.getBlockMetadata(x, y, z);
+        if(meta == 1 || meta == 3 || meta == 4 || meta == 15) //creep, stasis, explode delete
+           return null;
+        return super.getCollisionBoundingBoxFromPool(world, x, y, z);
+    }
+    
+    public void setBlockBoundsForItemRender()
+    {
+        this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
+    }
+    
+    /**
+     * Is this block (a) opaque and (b) a full 1m cube?  This determines whether or not to render the shared face of two
+     * adjacent blocks and also whether the player can attach torches, redstone wire, etc to this block.
+     */
+    public boolean isOpaqueCube()
+    {
+        return false;
+    }    
+    
+    
+    
     
     @Override
     public boolean isBlockSolid(IBlockAccess world, int x, int y, int z, int side) {
@@ -61,7 +96,7 @@ public class HoarFrost extends BlockIce {
         return metadata;
     }
     
-    @SuppressWarnings("rawtypes") 
+    @SuppressWarnings({ "rawtypes", "unchecked" }) 
     @SideOnly(Side.CLIENT)
     @Override
     public void getSubBlocks(Item par1, CreativeTabs tab, List subItems) {
@@ -110,12 +145,13 @@ public class HoarFrost extends BlockIce {
             world.scheduleBlockUpdate(x, y, z, this, this.tickRate(world)); //schedule for child
             int randomIndex = random.nextInt(Vector3.conductanceNeighbors.length);
             WorldXYZ randomNeighbor = new WorldXYZ(world, x, y, z).offset(Vector3.conductanceNeighbors[randomIndex]);
-            if(randomNeighbor.getBlock().equals(Blocks.air) || randomNeighbor.getBlock().equals(Blocks.tallgrass)) {
+            Block nBlock = randomNeighbor.getBlock();
+            if(nBlock.equals(Blocks.air) || nBlock.equals(Blocks.tallgrass) || nBlock.equals(Blocks.double_plant)) {
                 ArrayList<WorldXYZ> indirectNeighbors = randomNeighbor.getDirectNeighbors();
                 for(WorldXYZ base : indirectNeighbors) {
                     Block block = base.getBlock();
                     if( !block.equals(ModBlock.hoar_frost) && !block.equals(Blocks.air) ) { //found a valid growth location
-                        if(random.nextInt(800) == 1) //important to limit the exponential growth of creep
+                        if(random.nextInt(1000) == 1) //important to limit the exponential growth of creep
                             growthMode = 3; //stasis mode
                         randomNeighbor.setBlock(ModBlock.hoar_frost, growthMode);
                         world.scheduleBlockUpdate(randomNeighbor.posX, randomNeighbor.posY, randomNeighbor.posZ, this, this.tickRate(world)); //schedule for child
--- src/main/java/com/newlinegaming/Runix/client/render/block/HoarFrostRenderer.java
+++ src/main/java/com/newlinegaming/Runix/client/render/block/HoarFrostRenderer.java
@@ -15,11 +15,6 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class HoarFrostRenderer implements ISimpleBlockRenderingHandler {
 
-    @SideOnly(Side.CLIENT)
-    private IIcon IconGreekFire;
-    @SideOnly(Side.CLIENT)
-    private IIcon IconGreekFire1;
-
     @Override
     public void renderInventoryBlock(Block block, int metadata, int modelId,
             RenderBlocks renderer) {
@@ -29,139 +24,98 @@ public class HoarFrostRenderer implements ISimpleBlockRenderingHandler {
     public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer) {
 
         Tessellator tessellator = Tessellator.instance;
-        IIcon icon = ModBlock.hoar_frost.getIcon(0, 1); //TODO individualize sides
-        IIcon icon1 = ModBlock.hoar_frost.getIcon(0, 0);
-        IIcon icon2 = icon;
+        int meta = world.getBlockMetadata(x, y, z);
+        if(meta == 0 || meta == 2 || meta == 14)
+            return renderer.renderStandardBlock(block, x, y, z); //still not rendering side blocks
+        IIcon icon = ModBlock.hoar_frost.getIcon(0, meta);
         boolean anyRender = false;
 
         tessellator.setColorOpaque_F(1.0F, 1.0F, 1.0F);
-        tessellator.setBrightness(ModBlock.hoar_frost.getMixedBrightnessForBlock(world, x, y, z));
-        double d0 = (double)icon2.getMinU();
-        double d1 = (double)icon2.getMinV();
-        double d2 = (double)icon2.getMaxU();
-        double d3 = (double)icon2.getMaxV();
+        tessellator.setBrightness(block.getMixedBrightnessForBlock(world, x, y, z));
+        double d0 = (double)icon.getMinU();
+        double d1 = (double)icon.getMinV();
+        double d2 = (double)icon.getMaxU();
+        double d3 = (double)icon.getMaxV();
         float f = 1.0F;
 
-        float f1 = 0.0625F;
-        float f2 = 0.0625F;
+        float f1 = 0.0125F;
 
-        if ((x + y + z & 1) == 1)
-        {
-            d0 = (double)icon1.getMinU();
-            d1 = (double)icon1.getMinV();
-            d2 = (double)icon1.getMaxU();
-            d3 = (double)icon1.getMaxV();
-        }
-
-        if ((x / 2 + y / 2 + z / 2 & 1) == 1)
-        {
-            double temp = d2;
-            d2 = d0;
-            d0 = temp;
-        }
-
-
-        if (ModBlock.hoar_frost.isBlockSolid(world, x - 1, y, z, 0))
+        if (block.isBlockSolid(world, x - 1, y, z, 0))
         {
             anyRender = true;
-            tessellator.addVertexWithUV((double)((float)x + f1), (double)((float)y + f + f2), (double)(z + 1), d2, d1);
-            tessellator.addVertexWithUV((double)(x + 0), (double)((float)(y + 0) + f2), (double)(z + 1), d2, d3);
-            tessellator.addVertexWithUV((double)(x + 0), (double)((float)(y + 0) + f2), (double)(z + 0), d0, d3);
-            tessellator.addVertexWithUV((double)((float)x + f1), (double)((float)y + f + f2), (double)(z + 0), d0, d1);
-            tessellator.addVertexWithUV((double)((float)x + f1), (double)((float)y + f + f2), (double)(z + 0), d0, d1);
-            tessellator.addVertexWithUV((double)(x + 0), (double)((float)(y + 0) + f2), (double)(z + 0), d0, d3);
-            tessellator.addVertexWithUV((double)(x + 0), (double)((float)(y + 0) + f2), (double)(z + 1), d2, d3);
-            tessellator.addVertexWithUV((double)((float)x + f1), (double)((float)y + f + f2), (double)(z + 1), d2, d1);
+            tessellator.addVertexWithUV((double)((float)x + f1), (double)((float)y + f + f1), (double)(z + 1), d2, d1);
+            tessellator.addVertexWithUV((double)(x + 0), (double)((float)(y + 0) + f1), (double)(z + 1), d2, d3);
+            tessellator.addVertexWithUV((double)(x + 0), (double)((float)(y + 0) + f1), (double)(z + 0), d0, d3);
+            tessellator.addVertexWithUV((double)((float)x + f1), (double)((float)y + f + f1), (double)(z + 0), d0, d1);
+            tessellator.addVertexWithUV((double)((float)x + f1), (double)((float)y + f + f1), (double)(z + 0), d0, d1);
+            tessellator.addVertexWithUV((double)(x + 0), (double)((float)(y + 0) + f1), (double)(z + 0), d0, d3);
+            tessellator.addVertexWithUV((double)(x + 0), (double)((float)(y + 0) + f1), (double)(z + 1), d2, d3);
+            tessellator.addVertexWithUV((double)((float)x + f1), (double)((float)y + f + f1), (double)(z + 1), d2, d1);
         }
 
-        if (ModBlock.hoar_frost.isBlockSolid(world, x + 1, y, z, 0))
+        if (block.isBlockSolid(world, x + 1, y, z, 0))
         {
             anyRender = true;
-            tessellator.addVertexWithUV((double)((float)(x + 1) - f1), (double)((float)y + f + f2), (double)(z + 0), d0, d1);
-            tessellator.addVertexWithUV((double)(x + 1 - 0), (double)((float)(y + 0) + f2), (double)(z + 0), d0, d3);
-            tessellator.addVertexWithUV((double)(x + 1 - 0), (double)((float)(y + 0) + f2), (double)(z + 1), d2, d3);
-            tessellator.addVertexWithUV((double)((float)(x + 1) - f1), (double)((float)y + f + f2), (double)(z + 1), d2, d1);
-            tessellator.addVertexWithUV((double)((float)(x + 1) - f1), (double)((float)y + f + f2), (double)(z + 1), d2, d1);
-            tessellator.addVertexWithUV((double)(x + 1 - 0), (double)((float)(y + 0) + f2), (double)(z + 1), d2, d3);
-            tessellator.addVertexWithUV((double)(x + 1 - 0), (double)((float)(y + 0) + f2), (double)(z + 0), d0, d3);
-            tessellator.addVertexWithUV((double)((float)(x + 1) - f1), (double)((float)y + f + f2), (double)(z + 0), d0, d1);
+            tessellator.addVertexWithUV((double)((float)(x + 1) - f1), (double)((float)y + f + f1), (double)(z + 0), d0, d1);
+            tessellator.addVertexWithUV((double)(x + 1 - 0), (double)((float)(y + 0) + f1), (double)(z + 0), d0, d3);
+            tessellator.addVertexWithUV((double)(x + 1 - 0), (double)((float)(y + 0) + f1), (double)(z + 1), d2, d3);
+            tessellator.addVertexWithUV((double)((float)(x + 1) - f1), (double)((float)y + f + f1), (double)(z + 1), d2, d1);
+            tessellator.addVertexWithUV((double)((float)(x + 1) - f1), (double)((float)y + f + f1), (double)(z + 1), d2, d1);
+            tessellator.addVertexWithUV((double)(x + 1 - 0), (double)((float)(y + 0) + f1), (double)(z + 1), d2, d3);
+            tessellator.addVertexWithUV((double)(x + 1 - 0), (double)((float)(y + 0) + f1), (double)(z + 0), d0, d3);
+            tessellator.addVertexWithUV((double)((float)(x + 1) - f1), (double)((float)y + f + f1), (double)(z + 0), d0, d1);
         }
 
-        if (ModBlock.hoar_frost.isBlockSolid(world, x, y, z - 1, 0))
+        if (block.isBlockSolid(world, x, y, z - 1, 0)) //north solid
         {
             anyRender = true;
-            tessellator.addVertexWithUV((double)(x + 0), (double)((float)y + f + f2), (double)((float)z + f1), d2, d1);
-            tessellator.addVertexWithUV((double)(x + 0), (double)((float)(y + 0) + f2), (double)(z + 0), d2, d3);
-            tessellator.addVertexWithUV((double)(x + 1), (double)((float)(y + 0) + f2), (double)(z + 0), d0, d3);
-            tessellator.addVertexWithUV((double)(x + 1), (double)((float)y + f + f2), (double)((float)z + f1), d0, d1);
-            tessellator.addVertexWithUV((double)(x + 1), (double)((float)y + f + f2), (double)((float)z + f1), d0, d1);
-            tessellator.addVertexWithUV((double)(x + 1), (double)((float)(y + 0) + f2), (double)(z + 0), d0, d3);
-            tessellator.addVertexWithUV((double)(x + 0), (double)((float)(y + 0) + f2), (double)(z + 0), d2, d3);
-            tessellator.addVertexWithUV((double)(x + 0), (double)((float)y + f + f2), (double)((float)z + f1), d2, d1);
+            tessellator.addVertexWithUV((double)(x + 0), (double)((float)y + f + f1), (double)((float)z + f1), d2, d1);
+            tessellator.addVertexWithUV((double)(x + 0), (double)((float)(y + 0) + f1), (double)(z + f1), d2, d3);
+            tessellator.addVertexWithUV((double)(x + 1), (double)((float)(y + 0) + f1), (double)(z + f1), d0, d3);
+            tessellator.addVertexWithUV((double)(x + 1), (double)((float)y + f + f1), (double)((float)z + f1), d0, d1);
+            tessellator.addVertexWithUV((double)(x + 1), (double)((float)y + f + f1), (double)((float)z + f1), d0, d1);
+            tessellator.addVertexWithUV((double)(x + 1), (double)((float)(y + 0) + f1), (double)(z + f1), d0, d3);
+            tessellator.addVertexWithUV((double)(x + 0), (double)((float)(y + 0) + f1), (double)(z + f1), d2, d3);
+            tessellator.addVertexWithUV((double)(x + 0), (double)((float)y + f + f1), (double)((float)z + f1), d2, d1);
         }
 
-        if (ModBlock.hoar_frost.isBlockSolid(world, x, y, z + 1, 0))
+        if (block.isBlockSolid(world, x, y, z + 1, 0))
         {
             anyRender = true;
-            tessellator.addVertexWithUV((double)(x + 1), (double)((float)y + f + f2), (double)((float)(z + 1) - f1), d0, d1);
-            tessellator.addVertexWithUV((double)(x + 1), (double)((float)(y + 0) + f2), (double)(z + 1 - 0), d0, d3);
-            tessellator.addVertexWithUV((double)(x + 0), (double)((float)(y + 0) + f2), (double)(z + 1 - 0), d2, d3);
-            tessellator.addVertexWithUV((double)(x + 0), (double)((float)y + f + f2), (double)((float)(z + 1) - f1), d2, d1);
-            tessellator.addVertexWithUV((double)(x + 0), (double)((float)y + f + f2), (double)((float)(z + 1) - f1), d2, d1);
-            tessellator.addVertexWithUV((double)(x + 0), (double)((float)(y + 0) + f2), (double)(z + 1 - 0), d2, d3);
-            tessellator.addVertexWithUV((double)(x + 1), (double)((float)(y + 0) + f2), (double)(z + 1 - 0), d0, d3);
-            tessellator.addVertexWithUV((double)(x + 1), (double)((float)y + f + f2), (double)((float)(z + 1) - f1), d0, d1);
+            tessellator.addVertexWithUV((double)(x + 1), (double)((float)y + f + f1), (double)((float)(z + 1) - f1), d0, d1);
+            tessellator.addVertexWithUV((double)(x + 1), (double)((float)(y + 0) + f1), (double)(z + 1 - 0), d0, d3);
+            tessellator.addVertexWithUV((double)(x + 0), (double)((float)(y + 0) + f1), (double)(z + 1 - 0), d2, d3);
+            tessellator.addVertexWithUV((double)(x + 0), (double)((float)y + f + f1), (double)((float)(z + 1) - f1), d2, d1);
+            tessellator.addVertexWithUV((double)(x + 0), (double)((float)y + f + f1), (double)((float)(z + 1) - f1), d2, d1);
+            tessellator.addVertexWithUV((double)(x + 0), (double)((float)(y + 0) + f1), (double)(z + 1 - 0), d2, d3);
+            tessellator.addVertexWithUV((double)(x + 1), (double)((float)(y + 0) + f1), (double)(z + 1 - 0), d0, d3);
+            tessellator.addVertexWithUV((double)(x + 1), (double)((float)y + f + f1), (double)((float)(z + 1) - f1), d0, d1);
         }
 
 
         double d5 = (double)x + 0.5D + 0.5D;
-        double d8 = (double)z + 0.5D - 0.5D;
         double d9 = (double)x + 0.5D - 0.5D;
-        double d11 = (double)z + 0.5D + 0.5D;
         d0 = (double)icon.getMinU();
         d1 = (double)icon.getMinV();
         d2 = (double)icon.getMaxU();
         d3 = (double)icon.getMaxV();
-        if (ModBlock.hoar_frost.isBlockSolid(world, x, y + 1, z, 0)) //block above
-        { //TODO not two textures
+        f = -0.0125F;
+        if (block.isBlockSolid(world, x, y + 1, z, 0)) {//block above
             anyRender = true;
             ++y;
-            f = -0.0625F;
-
-            if ((x + y + z & 1) == 0)
-            {
-                tessellator.addVertexWithUV(d9, (double)((float)y + f), (double)(z + 0), d2, d1);
-                tessellator.addVertexWithUV(d5, (double)(y + 0), (double)(z + 0), d2, d3);
-                tessellator.addVertexWithUV(d5, (double)(y + 0), (double)(z + 1), d0, d3);
-                tessellator.addVertexWithUV(d9, (double)((float)y + f), (double)(z + 1), d0, d1);
-            }
-            else
-            {
-                tessellator.addVertexWithUV((double)(x + 0), (double)((float)y + f), d11, d2, d1);
-                tessellator.addVertexWithUV((double)(x + 0), (double)(y + 0), d8, d2, d3);
-                tessellator.addVertexWithUV((double)(x + 1), (double)(y + 0), d8, d0, d3);
-                tessellator.addVertexWithUV((double)(x + 1), (double)((float)y + f), d11, d0, d1);
-            }
+            tessellator.addVertexWithUV(d9, (double)((float)y + f), (double)(z + 0), d2, d1);
+            tessellator.addVertexWithUV(d5, (double)(y + 0), (double)(z + 0), d2, d3);
+            tessellator.addVertexWithUV(d5, (double)(y + 0), (double)(z + 1), d0, d3);
+            tessellator.addVertexWithUV(d9, (double)((float)y + f), (double)(z + 1), d0, d1);
         }
 
-        if (ModBlock.hoar_frost.isBlockSolid(world, x, y - 1, z, 0)) { //bottom side
+        if (block.isBlockSolid(world, x, y - 1, z, 0)) { //bottom side
             anyRender = true;
-            f = 0.0625F;
-
-            if ((x + y + z & 1) == 0)
-            {
-                tessellator.addVertexWithUV(d9, (double)((float)y + f), (double)(z + 1), d0, d1);
-                tessellator.addVertexWithUV(d5, (double)(y + 0), (double)(z + 1), d0, d3);
-                tessellator.addVertexWithUV(d5, (double)(y + 0), (double)(z + 0), d2, d3);
-                tessellator.addVertexWithUV(d9, (double)((float)y + f), (double)(z + 0), d2, d1);
-            }
-            else
-            {
-                tessellator.addVertexWithUV((double)(x + 1), (double)((float)y + f), d11, d0, d1);
-                tessellator.addVertexWithUV((double)(x + 1), (double)(y + 0), d8, d0, d3);
-                tessellator.addVertexWithUV((double)(x + 0), (double)(y + 0), d8, d2, d3);
-                tessellator.addVertexWithUV((double)(x + 0), (double)((float)y + f), d11, d2, d1);
-            }
+            f = -f;
+            tessellator.addVertexWithUV(d9, (double)((float)y + f), (double)(z + 1), d0, d1);
+            tessellator.addVertexWithUV(d5, (double)(y + f), (double)(z + 1), d0, d3);
+            tessellator.addVertexWithUV(d5, (double)(y + f), (double)(z + 0), d2, d3);
+            tessellator.addVertexWithUV(d9, (double)((float)y + f), (double)(z + 0), d2, d1);
         }
 
         return anyRender;
@@ -169,7 +123,7 @@ public class HoarFrostRenderer implements ISimpleBlockRenderingHandler {
 
     @Override
     public boolean shouldRender3DInInventory(int modelId) {
-        return true;
+        return false;
     }
 
     @Override
